1d862a4efc62427c0ab6b1ec6ecb3d3248cf5ede
HanByul Yang <hanbyul.yang@gmail.com>
2022-11-06 18:12:31 +0900

    Model Reporting

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from flask import Flask, session, jsonify, request
 import pandas as pd
 import numpy as np
 import pickle
-import create_prediction_model
-import diagnosis 
-import predict_exited_from_saved_model
 import json
 import os
 
+from diagnostics import {
+    model_predictions,
+    dataframe_summary,
+    execution_time,
+    missing_data,
+    outdated_packages_list
+}
+from scoring import score_model
 
 
 ######################Set up variables for use in our script
@@ -19,32 +24,49 @@ with open('config.json','r') as f:
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 
-prediction_model = None
+prediction_model = pickle.load(open(os.path.join(os.getcwd(), config['prod_deployment_path'], "trainedmodel.pkl"), "rb"))
 
 
 #######################Prediction Endpoint
 @app.route("/prediction", methods=['POST','OPTIONS'])
 def predict():        
     #call the prediction function you created in Step 3
-    return #add return value for prediction outputs
+    filename = request.args.get('inputdata')
+    # print(filename)
+    file_path = os.path.join(os.getcwd(), filename)
+    if os.path.isfile(file_path) is False:
+        return f"{filename} doesn't exist"
+
+    pred = model_predictions(file_path)
+    return str(pred) #add return value for prediction outputs
 
 #######################Scoring Endpoint
 @app.route("/scoring", methods=['GET','OPTIONS'])
-def stats():        
+def scoring():        
     #check the score of the deployed model
-    return #add return value (a single F1 score number)
+    f1score = score_model()
+    return str(f1score) #add return value (a single F1 score number)
 
 #######################Summary Statistics Endpoint
 @app.route("/summarystats", methods=['GET','OPTIONS'])
 def stats():        
     #check means, medians, and modes for each column
-    return #return a list of all calculated summary statistics
+    statistics = dataframe_summary()
+    return str(statistics) #return a list of all calculated summary statistics
 
 #######################Diagnostics Endpoint
 @app.route("/diagnostics", methods=['GET','OPTIONS'])
-def stats():        
+def diagnostics():        
     #check timing and percent NA values
-    return #add return value for all diagnostics
+    timing = execution_time()
+    missing_data = missing_data()
+    dependencies = outdated_packages_list()
+    res = {
+        'timing' : timing,
+        'missing_data' : missing_data,
+        'dependency_check' : dependencies,
+    }
+    return str(res)#add return value for all diagnostics
 
 if __name__ == "__main__":    
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
--- reporting.py
+++ reporting.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import json
 import os
-
+import diagnostics
 
 
 ###############Load config.json and get path variables
@@ -15,7 +15,8 @@ with open('config.json','r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
-
+test_data_path = os.path.join(config['test_data_path']) 
+model_path = os.path.join(config['output_model_path'])
 
 
 
@@ -23,10 +24,21 @@ dataset_csv_path = os.path.join(config['output_folder_path'])
 def score_model():
     #calculate a confusion matrix using the test data and the deployed model
     #write the confusion matrix to the workspace
+    testdatacsv = os.path.join(os.getcwd(), test_data_path, 'testdata.csv')
+    testdata = pd.read_csv(testdatacsv)
+    # X = trainingdata.loc[:,['lastmonth_activity', 'lastyear_activity', 'number_of_employees']].values.reshape(-1, 3)
+    y_true = testdata['exited'].values
+    y_pred = diagnostics.model_predictions(testdatacsv)
+    # print(y_pred, y_true)
+    
+    cfm = metrics.confusion_matrix(y_true, y_pred)
+    # print(cfm)
 
-
-
-
+    classes = ["0", "1"]
+    df_cfm = pd.DataFrame(cfm, index = classes, columns = classes)
+    cfm_plot = sns.heatmap(df_cfm, annot=True)
+    cfm_path = os.path.join(os.getcwd(), model_path, 'confusionmatrix.png')
+    cfm_plot.figure.savefig(cfm_path)
 
 if __name__ == '__main__':
     score_model()
